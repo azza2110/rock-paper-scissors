5df891b3fa1b6d97608131db16b0aa2dd12acece
Aaron Brooker <aaronbrooker2110@gmail.com>
2023-04-18 22:06:54 +0100

    Remove old code in comments

--- script.js
+++ script.js
@@ -1,100 +1,9 @@
-/*
-function getPlayerChoice() {
-    let playerChoice;
-    playerChoice = prompt("What do you choose?");
-    playerChoice = playerChoice.toLowerCase();
-    if (playerChoice !== "rock" && playerChoice !== "scissors" && playerChoice !== "paper") {
-        alert("You have not made a valid selection. This game is invalid.");
-        playerChoice = null;
-    }
-    console.log(playerChoice);
-    return playerChoice;
-}
+let playerWinCount = 0
+let computerWinCount = 0;
 
-function playRound(playerSelection, computerSelection) {
-    let roundResult;
-    if (playerSelection == null) {
-        roundResult = "noResult";
-    } else if (playerSelection == computerSelection) {
-        roundResult = "tie";
-    } else if ( playerSelection == "rock" && computerSelection == "scissors" ||
-                playerSelection == "scissors" && computerSelection == "paper" ||
-                playerSelection == "paper" && computerSelection == "rock") {
-        roundResult = "playerWin"   ;
-    } else {
-        roundResult = "computerWin";
-    }
-    console.log(roundResult);
-    return roundResult
-}
-
-function game() {
-    let playerWinCount = 0
-    let computerWinCount = 0;
-    let roundResult;
-
-    roundResult = playRound(getPlayerChoice(),getComputerChoice());
-    if (roundResult == "playerWin") {
-        playerWinCount ++;
-    } else if (roundResult == "computerWin") {
-        computerWinCount ++;
-    } else if (roundResult == "tie") {
-    } else {
-    }
-    console.log(`Player: ${playerWinCount}, Computer: ${computerWinCount}`);
-    
-    roundResult = playRound(getPlayerChoice(),getComputerChoice());
-    if (roundResult == "playerWin") {
-        playerWinCount ++;
-    } else if (roundResult == "computerWin") {
-        computerWinCount ++;
-    } else if (roundResult == "tie") {
-    } else {
-    }
-    console.log(`Player: ${playerWinCount}, Computer: ${computerWinCount}`);
-    
-    roundResult = playRound(getPlayerChoice(),getComputerChoice());
-    if (roundResult == "playerWin") {
-        playerWinCount ++;
-    } else if (roundResult == "computerWin") {
-        computerWinCount ++;
-    } else if (roundResult == "tie") {
-    } else {
-    }
-    console.log(`Player: ${playerWinCount}, Computer: ${computerWinCount}`);
-    
-    roundResult = playRound(getPlayerChoice(),getComputerChoice());
-    if (roundResult == "playerWin") {
-        playerWinCount ++;
-    } else if (roundResult == "computerWin") {
-        computerWinCount ++;
-    } else if (roundResult == "tie") {
-    } else {
-    }
-    console.log(`Player: ${playerWinCount}, Computer: ${computerWinCount}`);
-    
-    roundResult = playRound(getPlayerChoice(),getComputerChoice());
-    if (roundResult == "playerWin") {
-        playerWinCount ++;
-    } else if (roundResult == "computerWin") {
-        computerWinCount ++;
-    } else if (roundResult == "tie") {
-    } else {
-    }
-    console.log(`Player: ${playerWinCount}, Computer: ${computerWinCount}`);
-
-    if (playerWinCount == computerWinCount) {
-        console.log(`Final score\nPlayer: ${playerWinCount}, Computer: ${computerWinCount}\nIt's a tie!`);
-    } else if (playerWinCount > computerWinCount) {
-        console.log(`Final score\nPlayer: ${playerWinCount}, Computer: ${computerWinCount}\nPlayer wins!`);
-    } else {
-        console.log(`Final score\nPlayer: ${playerWinCount}, Computer: ${computerWinCount}\nComputer wins!`);
-    }
-}
-
-game();
-
-*/
+const roundOutcome = document.getElementById('roundText');
+const gameScore = document.getElementById('scoreText');
+const gameOutcome = document.getElementById('gameText');
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
@@ -110,9 +19,6 @@ buttons.forEach((button) => {
         //button.addEventListener('click', myEvent => getPlayerChoice(myEvent, button.id));
     };
 });
-const roundOutcome = document.getElementById('roundText');
-const gameScore = document.getElementById('scoreText');
-const gameOutcome = document.getElementById('gameText');
 
 function getComputerChoice() {
     let computerChoice;
@@ -152,9 +58,6 @@ function playRound(playerSelection) {
         endGame();
     };
 };
-
-let playerWinCount = 0
-let computerWinCount = 0;
 
 function endGame(){
     let winner;
